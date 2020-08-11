61b7a81af5e2103e701160da361ef7efb70942db
layeredmind <sebastian.w94@hotmail.com>
2020-08-11 16:59:52 +0200

    basic architecture setup

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-import requests
+# This is the main entry-point for Alter Ego
+
+# IMPORT of necessary Libraries
 
 
-r = requests.get('https://en.wikipedia.org/wiki/Raspberry_Pi')
-print(r.text)
+# load Data from "project-settings.json" and store it as an object
+
+# connect to the two PIs and Initialize them (project-settings)
+
+# run "camera" on IN and OUT Pis
+
+# listen for pictures from IN and when new picture comes in run it through "face-recognition" API
+
+    # collect valid faces
+
+    # compress images 
+
+    # bundle images as training data
+
+    # send bundle to AI-Api for training
+
+# listen for api response and save output video to media-server
+
+    # download Video and take sample still-frames
+
+    # run samples through face recognition and pair face with video
+
+    # upload face with video pair
+
+# listen for pictures from OUT and when new picture comes in find matching face
+
+    # send media-server URL to PI and command "display"
+
---
+++ virtual/readme.md
@@ -0,0 +1,5 @@
+<h1> Virtual Pi Architecture </h1>
+
+<p> This is used as a documentation for the PI setup, handling and communication </p>
+<p> At the time of development there was no access to two working and properly connected Raspberry PIs.</p>
+<p> Once the draft is complete we need to move the content of "pi-input" onto the first "INPUT" pi and the content of "pi-output"onto the second "OUTPUT" pi </p>
