eff7f3a4ac8ef823de5df49e20202939c4a13a8e
layeredmind <sebastian.w94@hotmail.com>
2020-08-19 16:26:08 +0200

    Added NPM and improved camera script

--- virtual/pi-input/MyScripts/camera.py
+++ virtual/pi-input/MyScripts/camera.py
@@ -1,11 +1,32 @@
 from picamera import PiCamera
 from time import sleep
+import os
 
-camera = PiCamera()
+def make_padded_number(number):
+    fileNumberString = str(number)
+    return fileNumberString.zfill(4)
 
+def get_new_file_name(dir):
+    fileNumber = 0
+    while (os.path.isfile(dir + "image" + make_padded_number(fileNumber) + ".jpg")):
+        fileNumber += 1
+    return (dir + "image" + make_padded_number(fileNumber) + ".jpg")
+
+def take_picture(camera):
+    sleep(0.3)
+    print ("Taking a picture...")
+    path = get_new_file_name("MyPics/")
+    camera.capture(path)
+    
+camera = PiCamera(resolution=(1280, 720), framerate=30)
+camera.iso = 800
 camera.start_preview()
-for i in range(5):
-    sleep(5)
-    camera.capture('MyPics/image%s.jpg' % i)
-    print("Image shot!")
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+
+for i in range(10):
+    take_picture(camera)
+
 camera.stop_preview()
+
+
