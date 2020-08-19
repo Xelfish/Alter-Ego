084c1d5b72aa9f45d4464a95db68c12db3d9bccf
layeredmind <sebastian.w94@hotmail.com>
2020-08-19 23:39:27 +0200

    Added Initilization Task to set up the required folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,29 @@ const settings = require('./project-settings.json')
 const ftp = require('vinyl-ftp')
 const merge = require('merge-stream')
 
-function copyToPi(target){
+function connectToPi(target){
     const conn = ftp.create({
         host:     settings[target].ip,
         user:     settings[target].user,
         password: settings[target].password,
     })
+    return conn
+}
+
+function copyToPi(target){
+    
+    const conn = connectToPi(target)
 
     return src('./virtual/' + target + '/MyScripts/**')
         .pipe(conn.newer('/home/pi/MyScripts/'))
         .pipe(conn.dest('/home/pi/MyScripts/'))
+}
+
+function initPis(){
+
+    const conn = connectToPi("input-pi")
+    return src("./virtual/input-pi/*")
+    .pipe(conn.dest("/home/pi"))
 }
 
 function cleanPi(target){
@@ -38,3 +51,5 @@ exports.deploy = deployToInputPi
 exports.watch = watchScripts
 //TODO: Make clean task
 //exports.clean = parallel()
+//TODO: Make init task + vargs "--input , --output"
+exports.init = initPis
