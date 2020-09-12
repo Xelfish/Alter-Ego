f6a2df6d8504204660e99ba3cbec1541a6464b10
layeredmind <sebastian.w94@hotmail.com>
2020-09-13 00:13:07 +0200

    Implemented Videplay script for output-pi

---
+++ virtual/output-pi/MyScripts/play_video.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+
+VIDEONAME = sys.argv[1]
+
+def turnScreenOff():
+    subprocess.Popen(["vcgencmd", "display_power", "0"])
+
+def turnScreenOn():
+    subprocess.Popen(["vcgencmd", "display_power", "1"])
+
+def playVideo(video_path):
+    subprocess.Popen(["cvlc", "--fullscreen", "--intf", "dummy", "--play-and-exit", VIDEONAME])
+    pass
+
+def main(): 
+    turnScreenOn()
+    playVideo(VIDEONAME)
+    turnScreenOff()
+
+if __name__ == "__main__":
+    main()
