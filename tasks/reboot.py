e1a9d91475d781d91606112b13618c9f3c5e50da
layeredmind <sebastian.w94@hotmail.com>
2020-09-14 09:27:34 +0200

    Implemented reboot task

--- tasks/reboot.py
+++ tasks/reboot.py
@@ -6,12 +6,19 @@ from util.files import *
 
 settings = get_json_settings("project-settings.json")
 
-
+def restartPi(pi):
+    command = "sudo reboot"
+    print("restarting: ", pi["name"])
+    sshCommand(pi, command)
 
 def main():
-    #Implement Ask for input.
-    print(settings)
-    pass
+    pi = input("Which PI would you like to reboot?")
+    if (pi == "in"):
+        restartPi(settings["input-pi"])
+    elif (pi == "out"):
+        restartPi(settings["output-pi"])
+    else:
+        print("No valid pi. Aborting task...")
 
 if __name__ == "__main__":
     main()
